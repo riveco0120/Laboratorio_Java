152783c17f61d4e5b589e0159e1804391c5b92dc
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 16:44:05 -0500

    Metodos get, set y toString
    Se crearon los metodos set,get y toString de la clase serie

--- src/main/java/com/sofka/tallerejava/punto18/Serie.java
+++ src/main/java/com/sofka/tallerejava/punto18/Serie.java
@@ -23,4 +23,48 @@ public class Serie {
         this.genero = genero;
         this.creador = creador;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumeroTemporda() {
+        return numeroTemporda;
+    }
+
+    public void setNumeroTemporda(int numeroTemporda) {
+        this.numeroTemporda = numeroTemporda;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "titulo='" + titulo + '\'' +
+                ", numeroTemporda=" + numeroTemporda +
+                ", entregado=" + entregado +
+                ", genero='" + genero + '\'' +
+                ", creador='" + creador + '\'' +
+                '}';
+    }
+
 }
