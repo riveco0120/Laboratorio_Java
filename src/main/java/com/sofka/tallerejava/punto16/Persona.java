56934542bade94900d0e53483f509177df296982
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 15:06:44 -0500

    Metodos get y set
    Se implementaron los metodos get y set para la clase persona, y su tosString()

--- src/main/java/com/sofka/tallerejava/punto16/Persona.java
+++ src/main/java/com/sofka/tallerejava/punto16/Persona.java
@@ -106,6 +106,57 @@ public class Persona {
         return this.nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", DNI='" + DNI + '\'' +
+                ", sexo=" + sexo +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+
+
 }
 
 
