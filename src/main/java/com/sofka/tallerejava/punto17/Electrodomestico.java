fe51f6f04a3165b03ef3bf5bad411448908163fd
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 15:23:38 -0500

    Agregando clase electrodomestico
    Se agregaron los atributos y constructores de la clase electrodomestico

---
+++ src/main/java/com/sofka/tallerejava/punto17/Electrodomestico.java
@@ -0,0 +1,25 @@
+package com.sofka.tallerejava.punto17;
+
+public class Electrodomestico {
+    protected double precioBase=100;
+    protected String color ="Blanco";
+    protected char consumoEnergetico='F';
+    protected double peso=5;
+
+    public Electrodomestico() {
+    }
+
+    public Electrodomestico(double precioBase, double peso) {
+        this.precioBase = precioBase;
+        this.peso = peso;
+    }
+
+    public Electrodomestico(double precioBase, String color, char consumoEnergetico, double peso) {
+        this.precioBase = precioBase;
+        this.color = color;
+        this.consumoEnergetico = consumoEnergetico;
+        this.peso = peso;
+    }
+
+}
+
