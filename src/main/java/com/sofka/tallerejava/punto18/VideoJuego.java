d5a72169845f31e00c38e6925d986e8cd211acef
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 16:50:52 -0500

    Metodos get, set y toString
    Se crearon los metodos set,get y toString de la clase video juego

--- src/main/java/com/sofka/tallerejava/punto18/VideoJuego.java
+++ src/main/java/com/sofka/tallerejava/punto18/VideoJuego.java
@@ -1,4 +1,72 @@
 package com.sofka.tallerejava.punto18;
 
+import java.util.ArrayList;
+
 public class VideoJuego {
+
+    public String titulo="";
+    public int horasEstimada =10;
+    public boolean entregado=false;
+    public String genero="";
+    public String compañia="";
+    public ArrayList lista;
+
+    public VideoJuego() {
+
+    }
+
+    public VideoJuego(String titulo, int horasEstimada) {
+        this.titulo = titulo;
+        this.horasEstimada = horasEstimada;
+    }
+
+    public VideoJuego(String titulo, int horasEstimada, String genero, String compañia) {
+        this.titulo = titulo;
+        this.horasEstimada = horasEstimada;
+        this.genero = genero;
+        this.compañia = compañia;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHorasEstimada() {
+        return horasEstimada;
+    }
+
+    public void setHorasEstimada(int horasEstimada) {
+        this.horasEstimada = horasEstimada;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoJuego{" +
+                "titulo='" + titulo + '\'' +
+                ", horasEstimada=" + horasEstimada +
+                ", entregado=" + entregado +
+                ", genero='" + genero + '\'' +
+                ", compañia='" + compañia + '\'' +
+                '}';
+    }
 }
