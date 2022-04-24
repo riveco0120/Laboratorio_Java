f6616628cf203c7d7e43091941009b3024e9cbfe
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 16:41:26 -0500

    Clase serie
    Se agregan los atributos y constructores de la clase serie

--- src/main/java/com/sofka/tallerejava/punto18/Serie.java
+++ src/main/java/com/sofka/tallerejava/punto18/Serie.java
@@ -1,4 +1,26 @@
 package com.sofka.tallerejava.punto18;
 
 public class Serie {
+
+    private String titulo = "";
+    private int numeroTemporda = 3;
+    private boolean entregado = false;
+    private String genero = "";
+    private String creador = "";
+
+    public Serie() {
+
+    }
+
+    public Serie(String titulo, String creador) {
+        this.titulo = titulo;
+        this.creador = creador;
+    }
+
+    public Serie(String titulo, int numeroTemporda, String genero, String creador) {
+        this.titulo = titulo;
+        this.numeroTemporda = numeroTemporda;
+        this.genero = genero;
+        this.creador = creador;
+    }
 }
