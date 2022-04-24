103bd42c9835d9388021b75e7a9c2c3d937ef6cd
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 13:22:59 -0500

    Agregando funcionalidad texto
    Se agrego la funcionalidad para eliminar espacios

--- src/main/java/com/sofka/tallerejava/domaintexto/InformacionTextoI.java
+++ src/main/java/com/sofka/tallerejava/domaintexto/InformacionTextoI.java
@@ -2,4 +2,7 @@ package com.sofka.tallerejava.domaintexto;
 
 public interface InformacionTextoI {
     public String verificarDia();
+    public String cambiarLetra();
+    public String eliminarSpacios();
+    public String longitud();
 }
