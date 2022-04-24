ce785b75ab22e421a25cb1aa73d8d59fff189fb0
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-24 15:53:49 -0500

    Clase televisor
    Se agrego atributos y constructores de la clase televisor

--- src/main/java/com/sofka/tallerejava/punto17/Televisor.java
+++ src/main/java/com/sofka/tallerejava/punto17/Televisor.java
@@ -1,4 +1,21 @@
 package com.sofka.tallerejava.punto17;
 
-public class Televisor {
+public class Televisor extends Electrodomestico {
+    public double resolucion = 20;
+    public boolean sintonizadorTDT = false;
+
+    public Televisor() {
+
+    }
+
+    public Televisor(double precioBase, double peso) {
+        super(precioBase, peso);
+    }
+
+    public Televisor(double precioBase, String color, char consumoEnergetico, double peso, double resolucion, boolean sintonizadorTDT) {
+        super(precioBase, color, consumoEnergetico, peso);
+        this.resolucion = resolucion;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
 }
